09ebc2d06470587d6bb7af64776bd455616d7f55
FuckDoctors <1180450+FuckDoctors@users.noreply.github.com>
2022-04-09 16:34:41 +0000

    Deploying to master from @ FuckDoctors/notes2@101cdafb46c1a20e9d5e7560fb2881220db2a771 🚀

---
+++ assets/index.html.28a1d6f4.js
@@ -0,0 +1,31 @@
+const e={key:"v-b3a93a42",path:"/notes/frontend/vue/",title:"Vue \u7B14\u8BB0",lang:"zh-CN",frontmatter:{index:!0,icon:"vue",category:["\u7B14\u8BB0","Frontend","Vue"],tag:["vue"],summary:`# Vue \u7B14\u8BB0
+
+
+Vue2 \u5B66\u4E60\u7B14\u8BB0
+\u53EA\u662F\u7167\u7740\u5B98\u65B9\u6307\u5357\u6572\u4E86\u6572\u4EE3\u7801
+
+
+Vue3 \u5B66\u4E60\u7B14\u8BB0
+\u53EA\u662F\u7167\u7740\u5B98\u65B9\u6307\u5357\u6572\u4E86\u6572\u4EE3\u7801
+
+
+Vue.js \u8BBE\u8BA1\u4E0E\u5B9E\u73B0
+\u672C\u4E66\u6E90\u4EE3\u7801: https://github.com/HcySunYang/code-for-vue-3-book
+
+
+`,head:[["meta",{property:"og:url",content:"https://zhaobc.site/notes/frontend/vue/"}],["meta",{property:"og:site_name",content:"\u8D75\u658C\u7684\u5C0F\u7AD9"}],["meta",{property:"og:title",content:"Vue \u7B14\u8BB0"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:locale",content:"zh-CN"}],["meta",{property:"og:locale:alternate",content:"en-US"}],["meta",{property:"article:tag",content:"vue"}]]},excerpt:`<h1 id="vue-\u7B14\u8BB0" tabindex="-1"><a class="header-anchor" href="#vue-\u7B14\u8BB0" aria-hidden="true">#</a> Vue \u7B14\u8BB0</h1>
+<ul>
+<li>
+<p><a href="https://github.com/FuckDoctors/hello-vue-2.0" target="_blank" rel="noopener noreferrer">Vue2 \u5B66\u4E60\u7B14\u8BB0<ExternalLinkIcon/></a></p>
+<p>\u53EA\u662F\u7167\u7740\u5B98\u65B9\u6307\u5357\u6572\u4E86\u6572\u4EE3\u7801</p>
+</li>
+<li>
+<p><a href="https://github.com/FuckDoctors/hello-vue3" target="_blank" rel="noopener noreferrer">Vue3 \u5B66\u4E60\u7B14\u8BB0<ExternalLinkIcon/></a></p>
+<p>\u53EA\u662F\u7167\u7740\u5B98\u65B9\u6307\u5357\u6572\u4E86\u6572\u4EE3\u7801</p>
+</li>
+<li>
+<p><RouterLink to="/notes/frontend/vue/vue3-book/">Vue.js \u8BBE\u8BA1\u4E0E\u5B9E\u73B0</RouterLink></p>
+<p>\u672C\u4E66\u6E90\u4EE3\u7801: <a href="https://github.com/HcySunYang/code-for-vue-3-book" target="_blank" rel="noopener noreferrer">https://github.com/HcySunYang/code-for-vue-3-book<ExternalLinkIcon/></a></p>
+</li>
+</ul>
+`,headers:[],git:{createdTime:1649521628e3,updatedTime:1649521628e3,contributors:[{name:"FuckDoctors",email:"zhbchwin@163.com",commits:1}]},readingTime:{minutes:.24,words:73},filePathRelative:"notes/frontend/vue/README.md"};export{e as data};

e09132b1c519aea385fce0163ae5b4572586f1aa
FuckDoctors <1180450+FuckDoctors@users.noreply.github.com>
2022-05-02 06:20:38 +0000

    Deploying to master from @ FuckDoctors/notes2@74fe367838680d2f55828b08ce5cffe021c96c8e 🚀

---
+++ assets/index.html.e8b688fa.js
@@ -0,0 +1,13 @@
+const t={key:"v-ac39303c",path:"/en/notes/frontend/",title:"Frontend",lang:"en-US",frontmatter:{index:10,dir:{text:"Frontend",index:10},icon:"template",category:["notes","frontend"],tag:["js","ts","html","css","vue"],summary:`# Frontend
+
+JavaScript
+TypeScript
+Vue
+
+`,head:[["meta",{property:"og:url",content:"https://zhaobc.site/en/notes/frontend/"}],["meta",{property:"og:site_name",content:"ZhaoBin's site"}],["meta",{property:"og:title",content:"Frontend"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:updated_time",content:"2022-05-02T06:12:00.000Z"}],["meta",{property:"og:locale",content:"en-US"}],["meta",{property:"og:locale:alternate",content:"zh-CN"}],["meta",{property:"article:tag",content:"js"}],["meta",{property:"article:tag",content:"ts"}],["meta",{property:"article:tag",content:"html"}],["meta",{property:"article:tag",content:"css"}],["meta",{property:"article:tag",content:"vue"}],["meta",{property:"article:modified_time",content:"2022-05-02T06:12:00.000Z"}]]},excerpt:`<h1 id="frontend" tabindex="-1"><a class="header-anchor" href="#frontend" aria-hidden="true">#</a> Frontend</h1>
+<ul>
+<li><RouterLink to="/en/notes/frontend/js/">JavaScript</RouterLink></li>
+<li><RouterLink to="/en/notes/frontend/ts/">TypeScript</RouterLink></li>
+<li><RouterLink to="/en/notes/frontend/vue/">Vue</RouterLink></li>
+</ul>
+`,headers:[],git:{createdTime:165147192e4,updatedTime:165147192e4,contributors:[{name:"Zhao Bin",email:"413853119@qq.com",commits:1}]},readingTime:{minutes:.04,words:11},filePathRelative:"en/notes/frontend/README.md"};export{t as data};

8df8eeb150a1dc586dc143276d7958964df8fe73
FuckDoctors <1180450+FuckDoctors@users.noreply.github.com>
2022-05-02 15:06:37 +0000

    Deploying to master from @ FuckDoctors/notes2@f93fedb3808668ea53337c16a1ceee559132a3ab 🚀

---
+++ assets/index.html.442675c4.js
@@ -0,0 +1,13 @@
+const e={key:"v-cefc5a4a",path:"/en/notes/",title:"Notes",lang:"en-US",frontmatter:{index:!0,sticky:100,star:100,icon:"note",category:["notes"],summary:`# Notes
+
+\u7EB8\u4E0A\u5F97\u6765\u7EC8\u89C9\u6D45\uFF0C\u7EDD\u77E5\u6B64\u4E8B\u8981\u8EAC\u884C\u3002
+
+\u6709\u4E9B\u4E8B\u60C5\u770B\u8FC7\uFF0C\u611F\u89C9\u81EA\u5DF1\u77E5\u9053\uFF0C\u4F46\u662F\u8981\u7528\u7684\u65F6\u5019\u53C8\u4E0D\u8BB0\u5F97\u4E86\uFF0C\u6240\u4EE5\uFF0C\u5B66\u4E60\u4E0D\u80FD\u53EA\u770B\uFF0C\u8FD8\u8981\u52A8\u624B\uFF0C\u52A0\u6DF1\u5370\u8C61\u3002
+\u672C\u4ED3\u5E93\u7684\u76EE\u7684\u662F\u8BB0\u5F55\u5B66\u4E60\u7684\u4E1C\u897F\uFF0C\u8FB9\u5B66\u8FB9\u52A8\u624B\uFF0C\u7167\u7740\u5B66\u4E60\u8D44\u6599\u624B\u6284\u4E00\u4E0B\uFF0C\u4E0D\u7528\u5168\u6284\uFF0C\u8BB0\u5F55\u91CD\u70B9\u5185\u5BB9\u5373\u53EF\u3002
+`,head:[["meta",{property:"og:url",content:"https://zhaobc.site/en/notes/"}],["meta",{property:"og:site_name",content:"ZhaoBin's site"}],["meta",{property:"og:title",content:"Notes"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:updated_time",content:"2022-05-02T15:04:10.000Z"}],["meta",{property:"og:locale",content:"en-US"}],["meta",{property:"og:locale:alternate",content:"zh-CN"}],["meta",{property:"article:modified_time",content:"2022-05-02T15:04:10.000Z"}]]},excerpt:`<h1 id="notes" tabindex="-1"><a class="header-anchor" href="#notes" aria-hidden="true">#</a> Notes</h1>
+<blockquote>
+<p>\u7EB8\u4E0A\u5F97\u6765\u7EC8\u89C9\u6D45\uFF0C\u7EDD\u77E5\u6B64\u4E8B\u8981\u8EAC\u884C\u3002</p>
+</blockquote>
+<p>\u6709\u4E9B\u4E8B\u60C5\u770B\u8FC7\uFF0C\u611F\u89C9\u81EA\u5DF1\u77E5\u9053\uFF0C\u4F46\u662F\u8981\u7528\u7684\u65F6\u5019\u53C8\u4E0D\u8BB0\u5F97\u4E86\uFF0C\u6240\u4EE5\uFF0C\u5B66\u4E60\u4E0D\u80FD\u53EA\u770B\uFF0C\u8FD8\u8981\u52A8\u624B\uFF0C\u52A0\u6DF1\u5370\u8C61\u3002
+\u672C\u4ED3\u5E93\u7684\u76EE\u7684\u662F\u8BB0\u5F55\u5B66\u4E60\u7684\u4E1C\u897F\uFF0C\u8FB9\u5B66\u8FB9\u52A8\u624B\uFF0C\u7167\u7740\u5B66\u4E60\u8D44\u6599\u624B\u6284\u4E00\u4E0B\uFF0C\u4E0D\u7528\u5168\u6284\uFF0C\u8BB0\u5F55\u91CD\u70B9\u5185\u5BB9\u5373\u53EF\u3002</p>
+`,headers:[{level:2,title:"\u524D\u7AEF",slug:"\u524D\u7AEF",children:[]},{level:2,title:"\u540E\u7AEF",slug:"\u540E\u7AEF",children:[]},{level:2,title:"\u5176\u4ED6",slug:"\u5176\u4ED6",children:[]}],git:{createdTime:165150385e4,updatedTime:165150385e4,contributors:[{name:"Zhao Bin",email:"413853119@qq.com",commits:1}]},readingTime:{minutes:.76,words:227},filePathRelative:"en/notes/README.md"};export{e as data};

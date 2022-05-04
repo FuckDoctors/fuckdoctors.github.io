711cd8c82a7d97d4d3825f2cacf5601da79dad56
FuckDoctors <1180450+FuckDoctors@users.noreply.github.com>
2022-05-04 15:45:59 +0000

    Deploying to master from @ FuckDoctors/notes2@5650f54725e33c9d53a8289203387d0bb86b5331 🚀

---
+++ assets/index.html.5a5659b5.js
@@ -0,0 +1,17 @@
+const t={key:"v-43f82676",path:"/notes/frontend/",title:"\u524D\u7AEF\u7B14\u8BB0",lang:"zh-CN",frontmatter:{index:10,sidebarText:"\u6982\u8FF0",dir:{text:"\u524D\u7AEF\u7B14\u8BB0",index:10},icon:"template",category:["\u7B14\u8BB0","frontend"],tag:["js","ts","html","css","vue"],summary:`# \u524D\u7AEF\u7B14\u8BB0
+# \u6982\u8FF0
+\u8BB0\u5F55\u524D\u7AEF\u76F8\u5173\u7684\u5B66\u4E60\u7B14\u8BB0\u3002
+
+JavaScript
+TypeScript
+Vue
+
+`,head:[["meta",{property:"og:url",content:"https://zhaobc.site/notes/frontend/"}],["meta",{property:"og:site_name",content:"\u8D75\u658C\u7684\u5C0F\u7AD9"}],["meta",{property:"og:title",content:"\u524D\u7AEF\u7B14\u8BB0"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:updated_time",content:"2022-05-04T15:42:39.000Z"}],["meta",{property:"og:locale",content:"zh-CN"}],["meta",{property:"og:locale:alternate",content:"en-US"}],["meta",{property:"article:tag",content:"js"}],["meta",{property:"article:tag",content:"ts"}],["meta",{property:"article:tag",content:"html"}],["meta",{property:"article:tag",content:"css"}],["meta",{property:"article:tag",content:"vue"}],["meta",{property:"article:modified_time",content:"2022-05-04T15:42:39.000Z"}]]},excerpt:`<h1 id="\u524D\u7AEF\u7B14\u8BB0" tabindex="-1"><a class="header-anchor" href="#\u524D\u7AEF\u7B14\u8BB0" aria-hidden="true">#</a> \u524D\u7AEF\u7B14\u8BB0</h1>
+<h2 id="\u6982\u8FF0" tabindex="-1"><a class="header-anchor" href="#\u6982\u8FF0" aria-hidden="true">#</a> \u6982\u8FF0</h2>
+<p>\u8BB0\u5F55\u524D\u7AEF\u76F8\u5173\u7684\u5B66\u4E60\u7B14\u8BB0\u3002</p>
+<ul>
+<li><RouterLink to="/notes/frontend/js/">JavaScript</RouterLink></li>
+<li><RouterLink to="/notes/frontend/ts/">TypeScript</RouterLink></li>
+<li><RouterLink to="/notes/frontend/vue/">Vue</RouterLink></li>
+</ul>
+`,headers:[{level:2,title:"\u6982\u8FF0",slug:"\u6982\u8FF0",children:[]}],git:{createdTime:1651678959e3,updatedTime:1651678959e3,contributors:[{name:"Zhao Bin",email:"413853119@qq.com",commits:1}]},readingTime:{minutes:.09,words:27},filePathRelative:"notes/frontend/README.md"};export{t as data};

1db9516e4e62056f22b7eba2bdc54e64e7396c11
FuckDoctors <1180450+FuckDoctors@users.noreply.github.com>
2023-02-05 15:52:08 +0000

    Deploying to master from @ FuckDoctors/notes2@ee2e97a44a8b3f83fda8ea5224f1db101bbf7cdc 🚀

--- assets/mermaid-mindmap.esm.min-5dec4de7.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./app-99d532d9.js";import"./framework-f53d5b48.js";const t="mindmap",i=a=>a.match(/^\s*mindmap/)!==null,o=async()=>{const{diagram:a}=await r(()=>import("./diagram-definition.0faef4c2-4dda171c.js"),[]);return{id:t,diagram:a}},e={id:t,detector:i,loader:o};export{e as default};

1ad854ce82ac1aefd14f244215d7b41bea350b1f
FuckDoctors <1180450+FuckDoctors@users.noreply.github.com>
2023-02-28 15:20:00 +0000

    Deploying to master from @ FuckDoctors/notes2@a0ef6b0f3e87bc24f480fbff9ccf835a6da2bda4 🚀

---
+++ assets/errorDiagram-c771b856-10c7dd52.js
@@ -0,0 +1,1 @@
+import{aB as r}from"./mermaid.core-09dc680d.js";import"./app-ee1914db.js";import"./framework-bf49341f.js";const e=()=>"",t=e,p={db:{clear:()=>{}},styles:t,renderer:r,parser:{parser:{yy:{}},parse:()=>{}},init:()=>{}};export{p as diagram};

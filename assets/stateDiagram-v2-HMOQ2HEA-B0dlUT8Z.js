71629392254dfffefb11c1583758b9b5b24555ac
FuckDoctors <413853119@qq.com>
2025-01-26 10:38:22 +0000

    Deploying to master from @ FuckDoctors/notes2@f0a23d8e1e293cc2294d44b560c78ff465c108d4 🚀

---
+++ assets/stateDiagram-v2-HMOQ2HEA-B0dlUT8Z.js
@@ -0,0 +1,1 @@
+import{I as s,r as a,B as t,a as e}from"./chunk-LDJ5YOBB-BoRasQYd.js";import"./chunk-P7KHIEPL-CqMomUcy.js";import{m as o}from"./mermaid.esm.min-CKCwNdH6.js";import"./app-4FyL7Bd-.js";var l={parser:s,db:a,renderer:t,styles:e,init:o(r=>{r.state||(r.state={}),r.state.arrowMarkerAbsolute=r.arrowMarkerAbsolute,a.clear()},"init")};export{l as diagram};
